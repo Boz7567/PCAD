d3900abc371501b9997106810daae9d48870e007
Boz7567 <aleboz.asquasciati@gmail.com>
2026-03-09 12:45:20 +0100

    done punto 1 (?)

--- lab1/philo.c
+++ lab1/philo.c
@@ -4,49 +4,65 @@
 #include <unistd.h>
 
 #define N_FILOSOFI 5
+#define NUM_PASTI 5
 
-const int id[N_FILOSOFI] = {0,1,2,3,4};
+pthread_t filosofi[N_FILOSOFI];
+pthread_mutex_t bacchette[N_FILOSOFI];
 
+int id[N_FILOSOFI] = {0,1,2,3,4};
 
-void *test(void* a);
+void* filosofo(void* arg){
+    int i = *(int*)arg;
+
+    for(int pasto=0; pasto<NUM_PASTI; pasto++){
+
+        printf("Filosofo %d: sta pensando\n", i);
+        // sleep(rand()%4);
+
+        // tempo uguale per tutti, per mostrare il caso di deadlock        
+        sleep(1);
+
+        pthread_mutex_lock(&bacchette[i]);
+        printf("Filosofo %d: ha la bacchetta sinistra\n", i);
+        sleep(1);
+
+        pthread_mutex_lock(&bacchette[(i+1)%N_FILOSOFI]);
+        printf("Filosofo %d: ha la bacchetta destra\n", i);
+
+        printf("Filosofo %d: sta mangiando\n", i);
+        sleep(2);
+
+        pthread_mutex_unlock(&bacchette[i]);
+        pthread_mutex_unlock(&bacchette[(i+1)%N_FILOSOFI]);
+        printf("Filosofo %d: ha rilasciato le sue due bacchette\n", i);
+
+        sleep(1);
+    }
+
+    return NULL;
+}
 
 int main(){
-
-    // int* id = malloc(sizeof(int) * N_FILOSOFI);
-    // for(int i = 0; i<N_FILOSOFI; i++){
-    //     id[i] = i+1;
-    // }
-
-
-    pthread_t* filosofi;
-
-    filosofi = malloc(sizeof(pthread_t) * N_FILOSOFI);
-
-    if(filosofi == NULL){
-        printf("Errore allocazione memoria\n");
-        return 1;
-    }
-    // printf("Memoria allocata correttamente\n");
-
-    for(int i = 0; i < N_FILOSOFI; i++){
-        if(pthread_create(&filosofi[i], NULL, test, &id[i]) != 0)
-        {
-            printf("palle");
-            return 1;
-        }   
+    // srand(time(NULL));
+    for(int i=0; i<N_FILOSOFI; i++){
+        pthread_mutex_init(&bacchette[i], NULL);
     }
 
-    for(int i = 0; i < N_FILOSOFI; i++){
+    for(int i=0; i<N_FILOSOFI; i++){
+        pthread_create(&filosofi[i], NULL, filosofo, &id[i]);
+    }
+
+    for(int i=0; i<N_FILOSOFI; i++){
         pthread_join(filosofi[i], NULL);
     }
 
-    free(filosofi);
+    for(int i=0; i<N_FILOSOFI; i++){
+        pthread_mutex_destroy(&bacchette[i]);
+    }
+
+    if(filosofi != NULL)
+        free(filosofi);
+    if(bacchette != NULL)
+        free(bacchette);
     return 0;
-}
-
-
-void* test(void* a){
-    int* s = (int *)a;
-    printf("Sono il filosofo %d\n", *s);
-
 }
