27a9b555fcf00de8d00090ad0d7f8506b26947d2
Boz7567 <aleboz.asquasciati@gmail.com>
2026-04-09 12:41:21 +0200

    glory to the ccp

--- lab2/es1/es1.c
+++ lab2/es1/es1.c
@@ -4,100 +4,100 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-// numeri arbitrari
 #define N_PASSEGGERI 10
 #define CAPIENZA_BUS 3
 
-
-struct bus{
+struct bus {
     pthread_t thread;
     unsigned int passeggeri;
     pthread_mutex_t lock;
     pthread_cond_t full;
     pthread_cond_t disponibile;
     pthread_cond_t lap;
-
+    pthread_cond_t empty;
     bool giro_finito;
 };
 
 pthread_t passeggeri[N_PASSEGGERI];
 struct bus Bus;
-
 int id[N_PASSEGGERI];
 
-void* passenger_func(void* arg){
+void* passenger_func(void* arg) {
     int id = *(int*)arg;
-    while(true){
+    while (1) {
         pthread_mutex_lock(&Bus.lock);
-        while(Bus.passeggeri >= CAPIENZA_BUS){
+
+        while (Bus.giro_finito || Bus.passeggeri >= CAPIENZA_BUS)
             pthread_cond_wait(&Bus.disponibile, &Bus.lock);
-        }
 
         Bus.passeggeri++;
-        printf("Passeggero %d Salito sul bus. (Posti occupati: %d/%d)\n", id, Bus.passeggeri, CAPIENZA_BUS);
+        printf("Passeggero %d salito sul bus. (Posti occupati: %d/%d)\n",
+               id, Bus.passeggeri, CAPIENZA_BUS);
 
-        if(Bus.passeggeri == CAPIENZA_BUS){
+        if (Bus.passeggeri == CAPIENZA_BUS) {
             pthread_cond_signal(&Bus.full);
             printf("Bus pieno!\n");
         }
 
-        while(!Bus.giro_finito)
+        while (!Bus.giro_finito)
             pthread_cond_wait(&Bus.lap, &Bus.lock);
 
         Bus.passeggeri--;
-        if(Bus.passeggeri == 0){
-            Bus.giro_finito = false;
-            pthread_cond_signal(&Bus.disponibile);
-        }
+        printf("Passeggero %d sceso dal bus. (Posti occupati: %d/%d)\n",
+               id, Bus.passeggeri, CAPIENZA_BUS);
+
+        if (Bus.passeggeri == 0)
+            pthread_cond_signal(&Bus.empty);
+
         pthread_mutex_unlock(&Bus.lock);
-        sleep(1);
+        usleep(100000);
     }
+    return NULL;
 }
 
-void* bus_func(void* arg){
-    // printf("pesozio\n");
-    while (true){
-        sleep(1);
-        pthread_mutex_lock(&Bus.lock);
-        while(Bus.passeggeri < CAPIENZA_BUS)
+void* bus_func(void* arg) {
+    pthread_mutex_lock(&Bus.lock);
+
+    while (1) {
+        Bus.giro_finito = false;
+        pthread_cond_broadcast(&Bus.disponibile);
+
+        while (Bus.passeggeri < CAPIENZA_BUS)
             pthread_cond_wait(&Bus.full, &Bus.lock);
 
-        Bus.giro_finito = false;
-        printf("\nFACCIO IL GIRO\n");
+        printf("\n--- FACCIO IL GIRO ---\n");
         pthread_mutex_unlock(&Bus.lock);
-        sleep(3); // giro du manubrio
+        sleep(3);
         pthread_mutex_lock(&Bus.lock);
-        printf("\nGIRO FATTO\n");
+        printf("--- GIRO FATTO ---\n\n");
+
         Bus.giro_finito = true;
         pthread_cond_broadcast(&Bus.lap);
-        
-        while(Bus.passeggeri > 0)
-            pthread_cond_wait(&Bus.disponibile, &Bus.lock);
 
-        pthread_mutex_unlock(&Bus.lock);
+        while (Bus.passeggeri > 0)
+            pthread_cond_wait(&Bus.empty, &Bus.lock);
     }
-    // exit(-1);
+    return NULL;
 }
 
-int main(){
+int main() {
     Bus.passeggeri = 0;
     Bus.giro_finito = false;
     pthread_mutex_init(&Bus.lock, NULL);
-    
     pthread_cond_init(&Bus.full, NULL);
     pthread_cond_init(&Bus.disponibile, NULL);
     pthread_cond_init(&Bus.lap, NULL);
-    
+    pthread_cond_init(&Bus.empty, NULL);
+
     pthread_create(&Bus.thread, NULL, bus_func, NULL);
-    
-    for(int i = 0; i<N_PASSEGGERI; i++){
+
+    for (int i = 0; i < N_PASSEGGERI; i++) {
         id[i] = i;
-    }
-    for(int i = 0; i<N_PASSEGGERI; i++){
         pthread_create(&passeggeri[i], NULL, passenger_func, &id[i]);
     }
 
-    for(int i = 0; i<N_PASSEGGERI; i++){
+    for (int i = 0; i < N_PASSEGGERI; i++)
         pthread_join(passeggeri[i], NULL);
-    }
+
+    return 0;
 }
