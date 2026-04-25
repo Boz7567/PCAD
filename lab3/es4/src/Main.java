a2788410356e2877d946dbb1942a44ee9e3ddfb1
gabriele traversa <s6072517@studenti.unige.it>
2026-04-25 10:36:27 +0200

    l'ho gasicchiata

--- lab3/es4/src/Main.java
+++ lab3/es4/src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("This is es4");
         RWext rw = new RWext();
-        int n = 10000; // numero di writer e reader
-        Thread[] arr = new Thread[2 * n];
-        for (int i = 0; i < 1000; i++) {
+        int n = 100; // numero di reader
+        Thread[] arr = new Thread[n * 3];
+        for (int i = 0; i < n*2; i++) {
             arr[i] = new Thread(new Writer(rw), "Writer-" + i);
         }
         for (int i = 0; i < n; i++) {
